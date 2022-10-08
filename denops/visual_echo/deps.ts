dd76f63c7da06de23a20348941267dde73dc2635
kamecha <masakikameyama81@gmail.com>
2022-10-08 09:06:04 +0900

    echoの叩き台を作成

--- denops/visual_echo/deps.ts
+++ denops/visual_echo/deps.ts
@@ -1,1 +1,4 @@
 export type { Denops } from "https://deno.land/x/denops_std@v2.0.0/mod.ts";
+export * from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
+export * as mapping from "https://deno.land/x/denops_std@v3.8.2/mapping/mod.ts";
+export * from "https://deno.land/x/denops_std@v3.8.2/mapping/types.ts";
--- denops/visual_echo/main.ts
+++ denops/visual_echo/main.ts
@@ -3,4 +3,33 @@ import {
 } from "./deps.ts";
 
 export async function main(denops: Denops): Promise<void> {
+
+	// vimのコマンドのマッピング
+	const maps = [
+		{
+			lhs: "<silent> <Plug>(visual_echo)",
+			rhs: ":echo<CR>",
+			mode: ["n", "v"],
+		},
+	];
+
+	for (const map of maps) {
+		await mapping.map(denops, map.lhs, map.rhs, {
+			mode: map.mode as Mode[],
+		});
+	}
+
+	denops.dispatcher = {
+		async echo(
+			bang: unknown,
+			start: unknown,
+			end: unknown,
+			arg: unknown,
+			): Promise<unkown> {
+			ensureNumber(start);
+			ensureNumber(end);
+			const text = await denops.call("getline", start, end);
+			console.log(text);
+		}
+	}
 }
